29486a7c5ff84aaa21919fa8548870585b0d5acd
2ieunn <lje040330@naver.com>
2025-07-31 15:31:00 +0900

    feat: UI 구현 및 Card 템플릿 연동

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         members: resolve(__dirname, 'members/index.html'),
+        topics: resolve(__dirname, 'topics/index.html'),
       },
     },
   },
