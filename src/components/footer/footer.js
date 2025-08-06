080337d973f2c623b28b3a60876fd78b23e9b449
2ieunn <lje040330@naver.com>
2025-08-06 14:21:30 +0900

    feat: footer 템플릿 생성
    -footer.js 생성 및 코드 작성
    -footer.scss 생성 및 코드 작성

---
+++ src/components/footer/footer.js
@@ -0,0 +1,9 @@
+var footer = document.querySelector('.footer');
+
+footer.innerHTML += `
+<div class="footer__txt">
+        <p>Contact us at: contact@irlab.edu</p>
+        <p>Follow us on social media</p>
+        <p>© 2023 IR Lab. All rights reserved.</p>
+      </div>
+`;
